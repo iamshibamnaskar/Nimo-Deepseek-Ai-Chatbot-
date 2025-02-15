dd30e448509aec1ae0d7490905aabff461478a88
iamshibamnaskar <83879123+iamshibamnaskar@users.noreply.github.com>
2025-02-15 18:55:05 +0530

    Update Main.jsx

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -50,7 +50,7 @@ const Main = () => {
     setShowResults(true)
 
     try {
-      const response = await fetch("https://customers-personals-cultural-configurations.trycloudflare.com/query", {
+      const response = await fetch("https://shibamnaskar.in/query", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
